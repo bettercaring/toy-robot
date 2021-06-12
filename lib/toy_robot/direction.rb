42592c568e7e9f55754c12ede72034bc520d9061
Anto Dominic <antodoms@gmail.com>
2021-06-12 15:43:25 +1000

    make direction entry file to be more expandable easily

--- lib/toy_robot/direction.rb
+++ lib/toy_robot/direction.rb
@@ -1,4 +1,6 @@
-require "toy_robot/directions/east"
-require "toy_robot/directions/north"
-require "toy_robot/directions/south"
-require "toy_robot/directions/west"
+# This is the entrypoint for all directions supported by the robot
+# More directions can easily be supported by creating a direction module
+# under `directions` folder. eg: north_east.rb, south_west.rb
+Dir[File.join(File.dirname(__FILE__), "directions/*.rb")].sort.each do |file|
+  require file
+end
